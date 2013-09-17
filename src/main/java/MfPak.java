6cc6efc3f06cee68293732a70bb17eb1908d254f
Mikis Seth Sørensen <mss@statsbiblioteket.dk>
2013-09-17 14:29:42 +0200

    Added API stub

---
+++ src/main/java/MfPak.java
@@ -0,0 +1,32 @@
+import java.util.List;
+import java.util.Map;
+
+import dk.statsbiblioteket.newspaper.processmonitor.datasources.Batch;
+import dk.statsbiblioteket.newspaper.processmonitor.datasources.DataSource;
+import dk.statsbiblioteket.newspaper.processmonitor.datasources.Event;
+
+public class MfPak implements DataSource {
+    private MfPakConfiguration configuration;
+    @Override
+    public List<Batch> getBatches(boolean includeDetails, Map<String, String> filters) {
+        return null;
+    }
+
+    @Override
+    public Batch getBatch(String batchID, boolean includeDetails) {
+        return null;
+    }
+
+    @Override
+    public Event getBatchEvent(String batchID, String eventID, boolean includeDetails) {
+        return null;
+    }
+
+    public MfPakConfiguration getConfiguration() {
+        return configuration;
+    }
+
+    public void setConfiguration(MfPakConfiguration configuration) {
+        this.configuration = configuration;
+    }
+}
---
+++ src/main/java/MfPakConfiguration.java
@@ -0,0 +1,26 @@
+public class MfPakConfiguration {
+    private String databaseUrl;
+    private String databaseUser;
+    private String databasePassword;
+
+    public String getDatabaseUrl() {
+        return databaseUrl;
+    }
+    public void setDatabaseUrl(String databaseUrl) {
+        this.databaseUrl = databaseUrl;
+    }
+
+    public String getDatabaseUser() {
+        return databaseUser;
+    }
+    public void setDatabaseUser(String databaseUser) {
+        this.databaseUser = databaseUser;
+    }
+
+    public String getDatabasePassword() {
+        return databasePassword;
+    }
+    public void setDatabasePassword(String databasePassword) {
+        this.databasePassword = databasePassword;
+    }
+}
