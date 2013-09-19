22cd584db57612a3edeb0d1872ffcc03f9087c1b
Colin Rosenthal <csr@statsbiblioteket.dk>
2013-09-19 13:55:09 +0200

    Reconfigured test to use testng and configurator.

--- src/test/java/dk/statsbiblioteket/newspaper/mfpakintegration/database/MfPakDAOTest.java
+++ src/test/java/dk/statsbiblioteket/newspaper/mfpakintegration/database/MfPakDAOTest.java
@@ -1,10 +1,13 @@
 package dk.statsbiblioteket.newspaper.mfpakintegration.database;
 
-import dk.statsbiblioteket.newspaper.mfpakintegration.MfPakConfiguration;
+import dk.statsbiblioteket.newspaper.mfpakintegration.configuration.MfPakConfiguration;
 import dk.statsbiblioteket.newspaper.processmonitor.datasources.Batch;
 import dk.statsbiblioteket.newspaper.processmonitor.datasources.Event;
-import org.junit.Test;
+import dk.statsbiblioteket.newspapers.mfpakintegration.ConfigurationProvider;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -21,18 +24,22 @@ import static org.junit.Assert.assertTrue;
  */
 public class MfPakDAOTest {
 
+    private MfPakConfiguration configuration;
+
+    @BeforeMethod(groups = {"integrationTest"})
+    public void loadConfiguration() throws IOException {
+        ConfigurationProvider configurationProvider = new ConfigurationProvider();
+        configuration = configurationProvider.loadConfiguration();
+    }
+
     /**
      * Test that we can get batches and events out of the database via the DAO. There should be at least fpour batches
      * in the database and at least one event each of types "Shipping" and "Received".
      * @throws Exception
      */
-    @Test
+    @Test(groups = {"integrationTest"})
     public void testGetAllBatches() throws Exception {
-        MfPakConfiguration config = new MfPakConfiguration();
-        config.setDatabaseUrl("jdbc:postgresql://achernar/mfpak-devel");
-        config.setDatabaseUser("mfpak");
-        config.setDatabasePassword("JJ1KM0bvVf");
-        MfPakDAO dao = new MfPakDAO(config);
+        MfPakDAO dao = new MfPakDAO(configuration);
         List<Batch> batches = dao.getAllBatches();
         assertTrue("Should have at least four batches", batches.size() > 3);
         int shipping = 0;
@@ -52,25 +59,17 @@ public class MfPakDAOTest {
         assertTrue("Should have at least one Received event", received > 0);
     }
 
-    @Test
+    @Test(groups = {"integrationTest"})
     public void testGetBatchByBarcode() throws SQLException {
-        MfPakConfiguration config = new MfPakConfiguration();
-        config.setDatabaseUrl("jdbc:postgresql://achernar/mfpak-devel");
-        config.setDatabaseUser("mfpak");
-        config.setDatabasePassword("JJ1KM0bvVf");
-        MfPakDAO dao = new MfPakDAO(config);
+        MfPakDAO dao = new MfPakDAO(configuration);
         Batch batch = dao.getBatchByBarcode("4004");
         assertNotNull("Should get non-null batch", batch);
         assertEquals("Batch should have two events.", 2, batch.getEventList().size());
     }
 
-    @Test
+    @Test(groups = {"integrationTest"})
     public void testGetEvent() throws SQLException {
-        MfPakConfiguration config = new MfPakConfiguration();
-        config.setDatabaseUrl("jdbc:postgresql://achernar/mfpak-devel");
-        config.setDatabaseUser("mfpak");
-        config.setDatabasePassword("JJ1KM0bvVf");
-        MfPakDAO dao = new MfPakDAO(config);
+        MfPakDAO dao = new MfPakDAO(configuration);
         Event event = dao.getEvent("4003", "Received");
         assertNotNull("Should have found this event.", event);
     }
