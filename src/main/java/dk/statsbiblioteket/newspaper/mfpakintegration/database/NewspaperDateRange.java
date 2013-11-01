affec519c2fb303d61e9c2f86d480669aa4f717b
ktc <ktc@statsbiblioteket.dk>
2013-11-01 13:42:35 +0100

    add NewspaperDataRange class, including test. Move InconsistentDatabaseException out of test code...

---
+++ src/main/java/dk/statsbiblioteket/newspaper/mfpakintegration/database/NewspaperDateRange.java
@@ -0,0 +1,57 @@
+package dk.statsbiblioteket.newspaper.mfpakintegration.database;
+
+import java.util.Date;
+
+
+/**
+ * Class to represent the date range of newspapers.
+ * Dates are full inclusive (contrary to joda-time), so that the from date and to date is both included.  
+ */
+public class NewspaperDateRange {
+
+    private final Date fromDate;
+    private final Date toDate;
+    
+    /**
+     * Constructor.
+     * @param from, the start date in the range (inclusive)
+     * @param to, the end date in the range (inclusive) 
+     */
+    public NewspaperDateRange(Date from, Date to) {
+        this.fromDate = from;
+        this.toDate = to;
+    }
+    
+    /**
+     * Gets the from date
+     * @return Date, the first date in the range 
+     */
+    public Date getFromDate() {
+        return fromDate;
+    }
+    
+    /**
+     * Gets the to date
+     * @return Date, the last date in the range 
+     */
+    public Date getToDate() {
+        return toDate;
+    }
+    
+    /**
+     * Determine if a date is included in the range. 
+     * The range is inclusive meaning that if date is either the fromDate or toDate, the date is included.
+     * @param date The date to check against the range.  
+     * @return true if the date is included, false otherwise  
+     */
+    public boolean isIncluded(Date date) {
+        boolean included = true;
+        
+        if(!((date.compareTo(fromDate) >= 0) && (date.compareTo(toDate) <= 0))) {
+            included = false;
+        }
+        
+        return included;
+    }
+    
+}
---
+++ src/test/java/dk/statsbiblioteket/newspaper/mfpakintegration/database/NewspaperDataRangeTest.java
@@ -0,0 +1,30 @@
+package dk.statsbiblioteket.newspaper.mfpakintegration.database;
+
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.*;
+
+import java.util.Date;
+
+public class NewspaperDataRangeTest {
+
+    @Test
+    public void testDataRange() {
+        Long t1 = 1234567890L;
+        Date fromDate = new Date(t1);
+        Date toDate = new Date(t1 + 100);
+        Date inBetweenDate = new Date(t1 + 50);
+        Date priorDate = new Date(t1 - 5);
+        Date afterDate = new Date(t1 + 110);
+        
+        NewspaperDateRange testRange = new NewspaperDateRange(fromDate, toDate);
+        assertEquals(fromDate, testRange.getFromDate());
+        assertEquals(toDate, testRange.getToDate());
+        assertTrue(testRange.isIncluded(inBetweenDate));
+        assertTrue(testRange.isIncluded(fromDate));
+        assertTrue(testRange.isIncluded(toDate));
+        assertFalse(testRange.isIncluded(priorDate));
+        assertFalse(testRange.isIncluded(afterDate));
+    }
+    
+}
