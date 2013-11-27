8f9b33e50075d3d0540b8a470a41d4cf9e22d09c
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2013-11-27 10:49:09 +0100

    Updated to newest batch event framework, and use it's ConfigConstants class for property names

--- src/main/java/dk/statsbiblioteket/newspaper/mfpakintegration/configuration/ConfigurationProperties.java
+++
@@ -1,7 +0,0 @@
-package dk.statsbiblioteket.newspaper.mfpakintegration.configuration;
-
-public class ConfigurationProperties {
-    public final static String DATABASE_URL = "mfpak.postgres.url";
-    public final static String DATABASE_USER = "mfpak.postgres.user";
-    public final static String DATABASE_PASSWORD = "mfpak.postgres.password";
-}
--- src/test/java/dk/statsbiblioteket/newspaper/mfpakintegration/database/ConfigurationProvider.java
+++ src/test/java/dk/statsbiblioteket/newspaper/mfpakintegration/database/ConfigurationProvider.java
@@ -1,13 +1,13 @@
 package dk.statsbiblioteket.newspaper.mfpakintegration.database;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
-
-import dk.statsbiblioteket.newspaper.mfpakintegration.configuration.ConfigurationProperties;
+import dk.statsbiblioteket.medieplatform.autonomous.ConfigConstants;
 import dk.statsbiblioteket.newspaper.mfpakintegration.configuration.MfPakConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 /**
  * Enables loading of the configuration from external folder to emulate configuration injection.
@@ -30,13 +30,13 @@ public class  ConfigurationProvider {
     private MfPakConfiguration createConfiguration (Properties properties) {
         MfPakConfiguration configuration = new MfPakConfiguration();
 
-        configuration.setDatabaseUrl(properties.getProperty(ConfigurationProperties.DATABASE_URL));
+        configuration.setDatabaseUrl(properties.getProperty(ConfigConstants.MFPAK_URL));
         assert (configuration.getDatabaseUrl() != null);
 
-        configuration.setDatabaseUser(properties.getProperty(ConfigurationProperties.DATABASE_USER));
+        configuration.setDatabaseUser(properties.getProperty(ConfigConstants.MFPAK_USER));
         assert (configuration.getDatabaseUser() != null);
 
-        configuration.setDatabasePassword(properties.getProperty(ConfigurationProperties.DATABASE_PASSWORD));
+        configuration.setDatabasePassword(properties.getProperty(ConfigConstants.MFPAK_PASSWORD));
         assert (configuration.getDatabasePassword() != null);
 
         return configuration;
