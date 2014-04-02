41978715c0c6c4801a7305bfeb0bfd00583ab49f
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2014-04-02 09:55:22 +0200

    Reverted some indentation changes

--- src/main/java/dk/statsbiblioteket/newspaper/mfpakintegration/database/MfPakDAO.java
+++ src/main/java/dk/statsbiblioteket/newspaper/mfpakintegration/database/MfPakDAO.java
@@ -28,21 +28,17 @@ public class MfPakDAO {
     protected DBConnector connector = null;
     private final MfPakConfiguration configuration;
     public static final String GET_BATCH_ID = "SELECT rowId FROM batch WHERE batchId=?";
-    public static final String GET_EVENTS
-            = "SELECT name, batchstatus.created FROM batchstatus, status WHERE batchstatus.statusrowId=status.rowId AND batchstatus.batchrowId=?";
+    public static final String GET_EVENTS = "SELECT name, batchstatus.created FROM batchstatus, status WHERE batchstatus.statusrowId=status.rowId AND batchstatus.batchrowId=?";
     public static final String GET_ALL_BARCODES = "SELECT  batchId, rowId FROM batch";
-    public static final String GET_ALL_EVENTS
-            = "SELECT batchrowId, name, batchstatus.created FROM batchstatus, status WHERE statusrowId = status.rowId ";
-    public static final String GET_NEWSPAPER_ID
-            = "SELECT NewsPaperId FROM NewsPaper WHERE RowId = (SELECT NewsPaperRowId FROM Batch WHERE BatchId = ?) ";
+    public static final String GET_ALL_EVENTS = "SELECT batchrowId, name, batchstatus.created from batchstatus, status where statusrowId = status.rowId ";
+    public static final String GET_NEWSPAPER_ID = "SELECT NewsPaperId FROM NewsPaper WHERE RowId = (SELECT NewsPaperRowId FROM Batch WHERE BatchId = ?) ";
 
     public MfPakDAO(MfPakConfiguration configuration) {
         this.configuration = configuration;
         try {
             Class.forName(configuration.getDatabaseDriver());
         } catch (ClassNotFoundException e) {
-            throw new RuntimeException(
-                    "Unable to find database driver '" + configuration.getDatabaseDriver() +
+            throw new RuntimeException("Unable to find database driver '" + configuration.getDatabaseDriver() +
                     "' in classpath.", e);
         }
     }
@@ -100,12 +96,10 @@ public class MfPakDAO {
      * Returns a batch with the given id (barcode) and all its relevant statuses, or null if no such batch exists.
      *
      * @param barcode
-     *
      * @return the batch.
      */
     public Batch getBatchByBarcode(String barcode) throws SQLException {
-        try (Connection con = getConnection();
-             PreparedStatement stmt = con.prepareStatement(GET_BATCH_ID)) {
+        try (Connection con = getConnection(); PreparedStatement stmt = con.prepareStatement(GET_BATCH_ID)) {
             stmt.setLong(1, Long.parseLong(barcode));//TODO parse errors?
             try (ResultSet rs = stmt.executeQuery()) {
                 boolean batchExists = rs.next();
@@ -145,13 +139,11 @@ public class MfPakDAO {
      * Returns the newspaper id for a batch with the given id (barcode), or null if no newspaper id is found.
      *
      * @param barcode for the batch
-     *
      * @return The id of the news paper found for batch with barcode.
      * @throws InconsistentDatabaseException if more than one newspaperID is found.
      */
     public String getNewspaperID(String barcode) throws SQLException, InconsistentDatabaseException {
-        try (Connection con = getConnection();
-             PreparedStatement stmt = con.prepareStatement(GET_NEWSPAPER_ID)) {
+        try (Connection con = getConnection(); PreparedStatement stmt = con.prepareStatement(GET_NEWSPAPER_ID)) {
             stmt.setLong(1, Long.parseLong(barcode));
             try (ResultSet rs = stmt.executeQuery()) {
                 boolean newspaperIDExists = rs.next();
@@ -173,20 +165,19 @@ public class MfPakDAO {
      * Method to look-up a newspaper entity information based on the newspaperID and a date.
      *
      * @param newspaperID the ID of the newspaper
-     * @param date        the date for which the entity information is wanted.
+     * @param date the date for which the entity information is wanted.
      *
      * @return NewspaperEntity the entity information about the newspaper. Null if no entity information can be
-     * found for a newspaperID on the given date
+     *      found for a newspaperID on the given date
      * @throws InconsistentDatabaseException if more than one NewspaperEntity is found
      */
-    public NewspaperEntity getNewspaperEntity(String newspaperID, Date date) throws
-                                                                             SQLException,
-                                                                             InconsistentDatabaseException {
-        final String selectSql
-                = "SELECT Name, PublicationLocation, FromDate, ToDate FROM NewsPaperTitle" + " WHERE NewsPaperRowId = (SELECT RowId FROM NewsPaper WHERE NewsPaperId = ?)" + " AND FromDate <= ?" + " AND ToDate >= ?";
+    public NewspaperEntity getNewspaperEntity(String newspaperID, Date date) throws SQLException, InconsistentDatabaseException {
+        final String selectSql = "SELECT Name, PublicationLocation, FromDate, ToDate FROM NewsPaperTitle"
+                + " WHERE NewsPaperRowId = (SELECT RowId FROM NewsPaper WHERE NewsPaperId = ?)"
+                + " AND FromDate <= ?"
+                + " AND ToDate >= ?";
         NewspaperEntity entity = null;
-        try (Connection con = getConnection();
-             PreparedStatement stmt = con.prepareStatement(selectSql)) {
+        try (Connection con = getConnection(); PreparedStatement stmt = con.prepareStatement(selectSql)) {
             stmt.setString(1, newspaperID);
             stmt.setDate(2, new java.sql.Date(date.getTime()));
             stmt.setDate(3, new java.sql.Date(date.getTime()));
@@ -200,8 +191,8 @@ public class MfPakDAO {
                     NewspaperDateRange range = new NewspaperDateRange(rs.getDate("FromDate"), rs.getDate("ToDate"));
                     entity.setNewspaperDateRange(range);
                     if (rs.next()) {
-                        throw new InconsistentDatabaseException(
-                                "Found more than one newspaper entity for newspaperID '" + newspaperID + "' on date '" + date + "'");
+                        throw new InconsistentDatabaseException("Found more than one newspaper entity for newspaperID '"
+                                + newspaperID + "' on date '" + date + "'");
                     }
                 } else {
                     log.warn("No newspaper entity for newspaperID '" + newspaperID + "' on date '" + date + "' found!");
@@ -215,28 +206,26 @@ public class MfPakDAO {
      * Method to get the list of valid NewspaperDateRanges for a given batchID.
      * The date ranges is sorted by from date ascending. The number of items in the list is the same
      * as the number of films in the given batch.
-     *
      * @param batchID The id of the batch to get the NewspaperDateRanges for.
-     *
      * @return The list of NewspaperDateRanges if any could be found, or null if none could be found.
-     * I.e. the list will never be empty.
+     *  I.e. the list will never be empty.
      */
     public List<NewspaperDateRange> getBatchDateRanges(String batchID) throws SQLException {
-        final String selectSql
-                = "SELECT FromDate, ToDate FROM Film" + " WHERE BatchRowId = (SELECT RowId FROM Batch WHERE BatchId = ?)" + " ORDER BY FromDate ASC";
+        final String selectSql = "SELECT FromDate, ToDate FROM Film"
+                + " WHERE BatchRowId = (SELECT RowId FROM Batch WHERE BatchId = ?)"
+                + " ORDER BY FromDate ASC";
         List<NewspaperDateRange> ranges;
-        try (Connection con = getConnection();
-             PreparedStatement stmt = con.prepareStatement(selectSql)) {
+        try (Connection con = getConnection(); PreparedStatement stmt = con.prepareStatement(selectSql)) {
             stmt.setLong(1, Long.parseLong(batchID));
-            try (ResultSet rs = stmt.executeQuery()) {
+            try(ResultSet rs = stmt.executeQuery()) {
                 ranges = new ArrayList<NewspaperDateRange>();
-                while (rs.next()) {
+                while(rs.next()) {
                     Date from = rs.getDate("FromDate");
                     Date to = rs.getDate("ToDate");
                     NewspaperDateRange range = new NewspaperDateRange(from, to);
                     ranges.add(range);
                 }
-                if (ranges.isEmpty()) {
+                if(ranges.isEmpty()) {
                     ranges = null;
                 }
             }
@@ -248,29 +237,35 @@ public class MfPakDAO {
     /**
      * Method to get the list of valid NewspaperEntities. The list of entities are all entities covered from
      * the earliest date on the films that the batch's is comprised of and to the last date.
-     *
      * @param batchID The ID of the batch to get titles for.
-     *
      * @return The list of NewspaperEntity if any could be found, or null if none could be found.
-     * I.e. the list will never be empty.
+     *  I.e. the list will never be empty.
      */
     public List<NewspaperEntity> getBatchNewspaperEntities(String batchID) throws SQLException {
         List<NewspaperEntity> entities = null;
 
-        final String selectSql
-                = "SELECT Name, FromDate, ToDate, PublicationLocation, NewsPaper.NewsPaperId" + " FROM NewsPaperTitle" + " JOIN NewsPaper ON NewsPaperTitle.NewsPaperRowId = NewsPaper.RowId" + " WHERE NewsPaperRowId = (SELECT NewsPaperRowId FROM Batch WHERE BatchId = ?)" + " AND (ToDate IS NULL OR ToDate >= (SELECT FromDate FROM Film" + " WHERE BatchRowId = (SELECT RowId FROM Batch WHERE BatchId = ?)" + " ORDER BY FromDate ASC LIMIT 1))" + " AND FromDate <= (SELECT ToDate FROM Film" + " WHERE BatchRowId = (SELECT RowId FROM Batch WHERE BatchId = ?)" + " ORDER BY ToDate DESC LIMIT 1)" + " ORDER BY FromDate ASC";
+        final String selectSql = "SELECT Name, FromDate, ToDate, PublicationLocation, NewsPaper.NewsPaperId"
+                + " FROM NewsPaperTitle"
+                + " JOIN NewsPaper ON NewsPaperTitle.NewsPaperRowId = NewsPaper.RowId"
+                + " WHERE NewsPaperRowId = (SELECT NewsPaperRowId FROM Batch WHERE BatchId = ?)"
+                + " AND (ToDate IS NULL OR ToDate >= (SELECT FromDate FROM Film"
+                    + " WHERE BatchRowId = (SELECT RowId FROM Batch WHERE BatchId = ?)"
+                    + " ORDER BY FromDate ASC LIMIT 1))"
+                + " AND FromDate <= (SELECT ToDate FROM Film"
+                    + " WHERE BatchRowId = (SELECT RowId FROM Batch WHERE BatchId = ?)"
+                    + " ORDER BY ToDate DESC LIMIT 1)"
+                + " ORDER BY FromDate ASC";
 
-        try (Connection con = getConnection();
-             PreparedStatement stmt = con.prepareStatement(selectSql)) {
+        try (Connection con = getConnection(); PreparedStatement stmt = con.prepareStatement(selectSql)) {
             stmt.setLong(1, Long.parseLong(batchID));
             stmt.setLong(2, Long.parseLong(batchID));
             stmt.setLong(3, Long.parseLong(batchID));
-            try (ResultSet rs = stmt.executeQuery()) {
+            try(ResultSet rs = stmt.executeQuery()) {
                 entities = new ArrayList<NewspaperEntity>();
-                while (rs.next()) {
+                while(rs.next()) {
                     Date from = rs.getDate("FromDate");
                     Date to = rs.getDate("ToDate");
-                    if (to == null) {
+                    if(to == null) {
                         to = new Date(Long.MAX_VALUE);
                     }
                     NewspaperDateRange range = new NewspaperDateRange(from, to);
@@ -281,7 +276,7 @@ public class MfPakDAO {
                     entity.setPublicationLocation(rs.getString("PublicationLocation"));
                     entities.add(entity);
                 }
-                if (entities.isEmpty()) {
+                if(entities.isEmpty()) {
                     entities = null;
                 }
             }
@@ -292,19 +287,20 @@ public class MfPakDAO {
 
     /**
      * Method to get the options ordered for the given batch
-     *
      * @param batchID the ID of the batch
-     *
      * @return {@link NewspaperBatchOptions} The options for the given batch.
      * @throws SQLException
      */
     public NewspaperBatchOptions getBatchOptions(String batchID) throws SQLException, InconsistentDatabaseException {
         NewspaperBatchOptions options = null;
-        final String selectSql
-                = "SELECT OptionB1, OptionB2, OptionB3, OptionB4," + " OptionB5, OptionB6, OptionB7, OptionB8, OptionB9 FROM OrderLine" + " WHERE RowId = (" + " SELECT OrderLineRowId FROM OrderBatch" + " WHERE BatchRowID = (" + " SELECT RowId FROM Batch WHERE BatchId = ?))";
+        final String selectSql = "SELECT OptionB1, OptionB2, OptionB3, OptionB4,"
+                + " OptionB5, OptionB6, OptionB7, OptionB8, OptionB9 FROM OrderLine"
+                + " WHERE RowId = ("
+                    + " SELECT OrderLineRowId FROM OrderBatch"
+                    + " WHERE BatchRowID = ("
+                        + " SELECT RowId FROM Batch where BatchId = ?))";
 
-        try (Connection con = getConnection();
-             PreparedStatement stmt = con.prepareStatement(selectSql)) {
+        try (Connection con = getConnection(); PreparedStatement stmt = con.prepareStatement(selectSql)) {
             stmt.setLong(1, Long.parseLong(batchID));
             try (ResultSet rs = stmt.executeQuery()) {
                 boolean newspaperOptionsExists = rs.next();
@@ -323,8 +319,8 @@ public class MfPakDAO {
                     options.setOptionB8(rs.getBoolean("OptionB8"));
                     options.setOptionB9(rs.getBoolean("OptionB9"));
                     if (rs.next()) {
-                        throw new InconsistentDatabaseException(
-                                "Found more than one set of batch options for batch '" + batchID + "'");
+                        throw new InconsistentDatabaseException("Found more than one set of batch options for batch '"
+                                + batchID + "'");
                     }
                 }
             }
@@ -335,20 +331,20 @@ public class MfPakDAO {
 
     /**
      * Retrieve the date that a batch was shipped from SB
-     *
      * @param batchID The ID of the batch
-     *
      * @return Date the date that the batch was shipped, null if no shipment has been registered
      * @throws InconsistentDatabaseException if more that one shipment date is found for the batch
      */
     public Date getBatchShipmentDate(String batchID) throws SQLException {
         Date shipmentDate = null;
 
-        final String selectSql
-                = "SELECT Created FROM BatchStatus" + " WHERE BatchRowId = (SELECT RowId FROM Batch" + " WHERE BatchId = ?)" + " AND StatusRowId = (SELECT RowId FROM Status" + " WHERE Name = 'Batch shipped to supplier');";
+        final String selectSql = "SELECT Created FROM BatchStatus"
+                + " WHERE BatchRowId = (SELECT RowId FROM Batch"
+                    + " WHERE BatchId = ?)"
+                + " AND StatusRowId = (SELECT RowId FROM Status"
+                    + " WHERE Name = 'Batch shipped to supplier');";
 
-        try (Connection con = getConnection();
-             PreparedStatement stmt = con.prepareStatement(selectSql)) {
+        try (Connection con = getConnection(); PreparedStatement stmt = con.prepareStatement(selectSql)) {
             stmt.setLong(1, Long.parseLong(batchID));
             try (ResultSet rs = stmt.executeQuery()) {
                 boolean shipmentDateFound = rs.next();
@@ -372,7 +368,6 @@ public class MfPakDAO {
      *
      * @param status           The name in the status table for this batch.
      * @param createdTimestamp
-     *
      * @return The corresponding event object.
      */
     private static Event createEvent(String status, Date createdTimestamp) {
@@ -390,7 +385,6 @@ public class MfPakDAO {
      *
      * @param batchBarcode
      * @param eventStatus
-     *
      * @return the event.
      */
     public Event getEvent(String batchBarcode, String eventStatus) throws SQLException {
