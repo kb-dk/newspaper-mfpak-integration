20b1a92f7b3ba030daa2f644187a06813006c69f
ktc <ktc@statsbiblioteket.dk>
2014-08-12 15:28:33 +0200

    add entries for the test batch '321123'

--- src/test/resources/test_data.sql
+++ src/test/resources/test_data.sql
@@ -274,6 +274,12 @@ UPDATE batchstatus SET statusrowId = (SELECT rowId from status WHERE "name"='Bat
 INSERT INTO batchstatus (statusrowId,batchrowId) SELECT 1,batchrowId from batchstatus WHERE rowId = LASTVAL();
 UPDATE batchstatus SET statusrowId = (SELECT rowId from status WHERE "name"='Batch received from supplier') WHERE rowId = LASTVAL();
 
+INSERT INTO batch (batchId, cartonnumber) values (321123, 205);
+INSERT INTO batchstatus (statusrowId, batchrowId) SELECT rowId, LASTVAL() from status WHERE name='Initial';
+INSERT INTO batchstatus (statusrowId, batchrowId) SELECT 1,batchrowId from batchstatus WHERE rowId = LASTVAL();
+UPDATE batchstatus SET statusrowId = (SELECT rowId from status WHERE "name"='Batch added to shipping container') WHERE rowId = LASTVAL();
+INSERT INTO batchstatus (statusrowId,batchrowId) SELECT 1,batchrowId from batchstatus WHERE rowId = LASTVAL();
+UPDATE batchstatus SET statusrowId = (SELECT rowId from status WHERE "name"='Batch shipped to supplier') WHERE rowId = LASTVAL();
 
 
 
